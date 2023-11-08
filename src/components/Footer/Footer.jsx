2e850b4c653db98b47ac4bcee23f967a7ba091a5
visinodeveloper <visinodeveloper@gmail.com>
2023-11-07 23:48:35 -0300

    need change :hover in links on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import {Link} from 'react-router-dom'
 import './footer.css';
 
 export default function Footer() {
   return (
     <div className="footer-container">
-      <a href="http://www.instagram.com/breskitchenn">
-        <i className="fa-brands fa-instagram fa-2xl"  style={{color: "#ed333b",}}></i>
-      </a>
+      <Link className="link-instagram" to="http://www.instagram.com/breskitchenn">
+      <p style={{color: "#ed333b",}}> <i className="fa-brands fa-instagram fa-2xl"  style={{color: "#ed333b",}}></i>
+      Instagram</p>
+      </Link>
 
 
-      <a href="https://wa.me/2215113411">
-      <i class="fa-brands fa-whatsapp fa-2xl" style={{color: "#26a269"}}></i>
-      </a>
+      <Link className="link-whatsapp" to="https://wa.me/2215113411">
+      <p style={{color: "#26a269"}}><i className="fa-brands fa-whatsapp fa-2xl" style={{color: "#26a269"}}></i>
+      Contacto</p>
+      </Link>
      
     </div>
   );
